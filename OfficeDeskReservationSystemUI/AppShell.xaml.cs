9662c610ea89f90a0beed15e9d9222eca0417a3d
Roman <168230669+dramonRog@users.noreply.github.com>
2026-03-29 13:22:54 +0200

    feat: add user interface project with initial data. Separate database, testing and generator logic into separate project class library

---
+++ OfficeDeskReservationSystemUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OfficeDeskReservationSystemUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
